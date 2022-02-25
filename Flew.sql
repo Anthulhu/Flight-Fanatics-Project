d1bdc52662a5f5f01011e5253ddfa10130431147
Adam <80983648+ShagFurnace@users.noreply.github.com>
2022-02-25 10:50:30 -0600

    Add files via upload
    psostgres table

---
+++ Flew.sql
@@ -0,0 +1,9 @@
+"id_number","departing_from","destination","depart_time","arrive_time","actual_depart_time","actual_arrive_time","airline"
+"AA445","Charlotte","Las Vegas","09:15:00-06:00","11:18:00-06:00","09:24:00-06:00","10:50:00-06:00","Amer Airl
+"
+"AA699","Phoenix
+","Philadelphia","08:05:00-06:00","02:21:00-06:00","08:14:00-06:00","02:01:00-06:00","Amer Airl
+"
+"DL2728","Atlanta","Wichita","09:15:00-06:00","10:40:00-06:00","09:28:00-06:00","10:32:00-06:00","Delta"
+"F9559","Raleigh-Durham
+","Denver","08:21:00-06:00","10:38:00-06:00","08:53:00-06:00","10:40:00-06:00","Frontier"
