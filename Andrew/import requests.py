3027a39ce4986464a675bfd7d77af11400dabddf
Andrew <62911610+Anthulhu@users.noreply.github.com>
2022-02-24 21:02:17 -0600

    Add files via upload

---
+++ Andrew/import requests.py
@@ -0,0 +1,22 @@
+import requests
+
+"""
+response = requests.get('https://crudcrud.com/api/33f89d745f3b49e7bc7a4b61470c6c3f')
+
+response.status_code
+    
+if response:
+    print('Success!')
+else:
+    print('An error has occurred.')
+"""
+
+requests.post('https://crudcrud.com/api/33f89d745f3b49e7bc7a4b61470c6c3f/cars')
+requests.post('https://crudcrud.com/api/33f89d745f3b49e7bc7a4b61470c6c3f/cars')
+requests.post('https://crudcrud.com/api/33f89d745f3b49e7bc7a4b61470c6c3f/cars')
+requests.post('https://crudcrud.com/api/33f89d745f3b49e7bc7a4b61470c6c3f/cars')
+requests.post('https://crudcrud.com/api/33f89d745f3b49e7bc7a4b61470c6c3f/cats')
+
+response = requests.get('https://crudcrud.com/api/33f89d745f3b49e7bc7a4b61470c6c3f')
+
+print(response)
