da5efa36dd947f2c7018bc812cdc37af9919c4ed
Andrew <62911610+Anthulhu@users.noreply.github.com>
2022-03-02 21:50:57 -0600

    Add files via upload

---
+++ Andrew/base_ui.py
@@ -0,0 +1,31 @@
+import random
+import tkinter as tk
+import requests
+
+def number():
+    flight = enter.get()
+    enter.delete(0, tk.END)
+    lbl_result["text"] = "Delay chance: " + str(random.randint(1, 100)) + "%"
+
+window = tk.Tk()
+window.title("Delay Prediction")
+greeting = tk.Label(
+    text="Enter your flight number",
+)
+greeting.pack()
+
+enter = tk.Entry()
+enter.pack()
+
+solve = tk.Button(
+    text="Click",
+    width=10,
+    command = number
+)
+solve.pack()
+lbl_result = tk.Label()
+lbl_result.pack()
+
+
+
+window.mainloop()
