f966beec20b59e8dafa63798347e6a9700d72f1f
Andrew <62911610+Anthulhu@users.noreply.github.com>
2022-03-31 21:19:09 -0500

    Update base_ui.py

--- Andrew/base_ui.py
+++ Andrew/base_ui.py
@@ -7,6 +7,7 @@ def number():
     enter.delete(0, tk.END)
     lbl_result["text"] = "Delay chance: " + str(random.randint(1, 100)) + "%"
 
+#creates the application Window
 window = tk.Tk()
 window.title("Delay Prediction")
 greeting = tk.Label(
@@ -14,14 +15,17 @@ greeting = tk.Label(
 )
 greeting.pack()
 
+#allows user to input their information
 enter = tk.Entry()
 enter.pack()
 
+#submit button for the information
 solve = tk.Button(
     text="Click",
     width=10,
     command = number
 )
+#output result
 solve.pack()
 lbl_result = tk.Label()
 lbl_result.pack()
